71e91ad5525620f54f9c80eeb69ede34e1cd2f7b
allen-yale <137728688@qq.com>
2018-05-31 18:06:22 +0800

    Initial commit

---
+++ src/com/allen/test/Test.java
@@ -0,0 +1,15 @@
+package com.allen.test;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(new Date());
+//            Thread.sleep(1000);D
+//        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello world");
+        }
+    }
+}
